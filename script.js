afc66dac255770abf6490b6c7527c8b75214e059
Federico Pozzi <117451142+federicoImmeLav@users.noreply.github.com>
2026-05-14 13:24:43 +0200

    a

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-
-document.getElementById('copy-iban').addEventListener('click', () => {
-  const ibanText = document.getElementById('iban-text').textContent.replace('Iban: ', '');
-  
-  navigator.clipboard.writeText(ibanText)
-    .then(() => {
-      alert('IBAN copiato!');
-    })
-    .catch(() => {
-      alert('Errore nel copiare l’IBAN');
-    });
-});
+function copyIBAN() {
+  const ibanText = document.getElementById('iban').innerText;
+  navigator.clipboard.writeText(ibanText).then(() => {
+    const message = document.getElementById('copy-message');
+    message.innerText = 'IBAN copiato!';
+    setTimeout(() => { message.innerText = ''; }, 2000);
+  });
+}
